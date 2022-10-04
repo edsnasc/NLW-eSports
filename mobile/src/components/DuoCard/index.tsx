0070dc6d3dee934fd1a42c823463daa2434b3b16
Edson Nascimento <edsnascimento89@gmail.com>
2022-10-04 17:03:01 -0300

    [new] : completing the game page

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,61 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { DuoInfo } from '../DuoInfo';
+
+import { styles } from './styles';
+import { THEME } from '../../theme';
+import { GameController } from 'phosphor-react-native';
+
+
+export interface DuoCardProps {
+  hourEnd: string;
+  hourStart: string;
+  id: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+  // onConnect: () => void;
+}
+
+export function DuoCard({ data }: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo
+        label="Nome"
+        value={data.name}
+      />
+      <DuoInfo
+        label="Tempo de jogo"
+        value={`${data.yearsPlaying} anos`}
+      />
+      <DuoInfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+      />
+      <DuoInfo
+        label="Chamada de audio"
+        value={data.useVoiceChannel ? "Sim" : "Não"}
+        colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        // onPress={onConnect}
+      >
+        <GameController
+          color={THEME.COLORS.TEXT}
+          size={20}
+        />
+
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,31 +1,89 @@
-import { useRoute } from '@react-navigation/native';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Entypo } from '@expo/vector-icons';
+import { useEffect, useState } from 'react'
 
-import { Background } from '../../components/Background';
+import logoImg from '../../assets/logo-nlw-esports.png';
+
 import { styles } from './styles';
 import { THEME } from '../../theme';
 
 import { GameParams } from '../../@types/navigation';
-import { TouchableOpacity, View } from 'react-native';
+
+import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { Text } from 'react-native';
 
 export default function Game() {
+  const [duos, setduos] = useState<DuoCardProps[]>([]);
 
-const route = useRoute();
-const game = route.params as GameParams;
+  const navigation = useNavigation();
+  const route = useRoute();
+  const game = route.params as GameParams;
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
+  useEffect(() => {
+    fetch(`http://192.168.18.59:3333/games/${game.id}/ads`)
+      .then(response => response.json())
+      .then(data => setduos(data));
+  }, []);
 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
         <View style={styles.header} >
-          <TouchableOpacity>
-            <Entypo 
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
               name="chevron-thin-left"
-              color={ THEME.COLORS.CAPTION_300 }
+              color={THEME.COLORS.CAPTION_300}
               size={20}
             />
           </TouchableOpacity>
+
+          <Image
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <View style={styles.right} />
         </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar"
+        />
+
+        <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <DuoCard 
+              data={item}
+              // onConnect={() => { }} 
+            />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
+        />
+
       </SafeAreaView>
     </Background>
   );
